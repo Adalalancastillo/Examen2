f3fc7bb86c1e6ccbbc6422f43cb0d832a6eb135f
Adalalancastillo <AdalAlanCastillo@gmail.com>
2022-07-14 23:17:12 -0700

    first commit

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    public function index()
+    {
+        $alumnos = Alumno::all();
+        return response([
+            'total_data' => count($alumnos),
+            'data' => $alumnos
+        ], 200);
+    }
+
+    public function alumnosMasculino()
+    {
+        $alumnos = Alumno::all();
+        return response()->json([
+            'total_data' => count($alumnos),
+            'data' => $alumnos
+        ], 200);
+
+        $data = $alumnos->where('genero', 'masculino');
+
+        return response()->json([
+            'genero'=>count($data)
+        ], 200);
+    }
+
+    public function alumnosFemenino()
+    {
+        $alumnos = Alumno::where('genero', 'femenino')->get();
+
+        $data = $alumnos->where('genero','==','femenino');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function sinbeca()
+    {
+        $alumnos = Alumno::where('becado', 'no')->get();
+
+        $data = $alumnos->where('becado','LIKE','no');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function becado()
+    {
+        $alumnos = Alumno::where('becado', 'si')->get();
+
+        $data = $alumnos->where('becado','LIKE','si');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function horarioM()
+    {
+        $alumnos = Alumno::where('horario', 'matutino')->get();
+
+        $data = $alumnos->where('horario','LIKE','matutino');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function horarioV()
+    {
+        $alumnos = Alumno::where('horario', 'vespertino')->get();
+
+        $data = $alumnos->where('horario','LIKE','vespertino');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function sinproblemaSalud()
+    {
+        $alumnos = Alumno::where('problema_de_salud', 'ninguno')->get();
+
+        $data = $alumnos->where('problema_de_salud','LIKE','ninguno');
+
+        $alumnos = Alumno::where('problema_de_salud', 'no')->get();
+
+        $data = $alumnos->where('problema_de_salud','LIKE','no');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function problemaSalud()
+    {
+        $alumnos = Alumno::where('problema_de_salud', 'si')->get();
+
+        $data = $alumnos->where('problema_de_salud','LIKE','si');
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    public function prepaDesaprobada()
+    {
+        $alumnos = Alumno::where('calificacion_prepa', '1')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '2')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '3')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '4')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '5')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '')->get();
+
+        return response()->json([
+            $alumnos
+        ], 200);
+    }
+
+    public function prepaAprobada()
+    {
+        $alumnos = Alumno::where('calificacion_prepa', '6')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '7')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '8')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '9')->get();
+        $alumnos = Alumno::where('calificacion_prepa', '10')->get();
+
+        return response()->json([
+            $alumnos
+        ], 200);
+    }
+
+
+    public function create(Request $request)
+    {
+        $data = $this->rules($request);
+        Alumno::create($data);
+        return response([
+            'message' => 'Se creo con exito el alumno'
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        $alumno = Alumno::where('id', $id)->first();
+        return response($alumno, 200);
+    }
+
+    public function update($id,Request $request)
+    {
+        $data = $this->rules($request);
+        Alumno::find($id)->fill($data)->save();
+        return response([
+            'message' => 'Se modifico con exito el alumno'
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        Alumno::find($id)->delete();
+        return response([
+            'message' => 'Se elimino con exito el alumno'
+        ], 200);
+    }
+
+    protected function rules($request)
+    {
+        return $request->validate([
+            'nombre'=>'required|string',
+            'edad'=>'required|string',
+            'genero'=>'required',
+            'carrera'=>'nullable',
+            'ednia_indigena'=>'required|string',
+            'horario'=>'required',
+            'calificacion_prepa'=>'required',
+            'becado'=>'required|string',
+            'problema_de_salud'=>'required|string'
+        ]);
+        //return $this->validate($request, [
+        //    'nombre'=>'required|string',
+        //    'edad'=>'required|string',
+        //    'genero'=>'required',
+        //    'carrera'=>'nullable',
+        //]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\AlumnoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/alumnos',[AlumnoController::class,'index']);
+
+Route::get('/alumnos/Masculino',[AlumnoController::class,'alumnosMasculino']);
+Route::get('/alumnos/Femenino',[AlumnoController::class,'alumnosFemenino']);
+
+Route::get('/alumnos/becados',[AlumnoController::class,'becado']);
+Route::get('/alumnos/sinbeca',[AlumnoController::class,'sinbeca']);
+
+Route::get('/alumnos/horario/matutino',[AlumnoController::class,'horarioM']);
+Route::get('/alumnos/horario/vespertino',[AlumnoController::class,'horarioV']);
+
+Route::get('/alumnos/problemasalud',[AlumnoController::class,'problemasalud']);
+Route::get('/alumnos/sinproblemasalud',[AlumnoController::class,'sinproblemasalud']);
+
+Route::get('/alumnos/prepa/aprobada',[AlumnoController::class,'prepaAprobada']);
+Route::get('/alumnos/prepa/desaprobada',[AlumnoController::class,'prepaDesaprobada']);
+
+Route::post('/alumnos',[AlumnoController::class,'create']);
+Route::put('/alumnos/{id}',[AlumnoController::class,'update']);
+Route::get('/alumnos/{id}',[AlumnoController::class,'show']);
+Route::delete('/alumnos/{id}',[AlumnoController::class,'delete']);
